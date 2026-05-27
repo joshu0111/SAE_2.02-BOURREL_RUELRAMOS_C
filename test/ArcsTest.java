7e8f3b11cdc2f2172cd2b4af49cf84b57e920b95
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-05-27 11:25:32 +0200

    Implementation de la class ArcsTest

---
+++ test/ArcsTest.java
@@ -0,0 +1,61 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+public class ArcsTest {
+
+    @Test
+    public void test1_constructeur_vide() {
+        // When
+        Arcs arcs = new Arcs();
+
+        // Then
+        assertNotNull(arcs.getListeArc(), "La liste d'arcs ne doit pas être nulle après initialisation.");
+        assertTrue(arcs.getListeArc().isEmpty(), "La liste d'arcs doit être vide à la création.");
+    }
+
+    @Test
+    public void test2_ajouterArc() {
+        // Given
+        Arcs arcs = new Arcs();
+        Arc arc1 = new Arc("B", 10.0);
+        Arc arc2 = new Arc("C", 5.5);
+
+        // When
+        arcs.ajouterArc(arc1);
+        arcs.ajouterArc(arc2);
+
+        // Then
+        ArrayList<Arc> liste = arcs.getListeArc();
+        assertEquals(2, liste.size(), "La liste devrait contenir exactement 2 arcs.");
+        assertEquals(arc1, liste.get(0), "Le premier arc ajouté n'est pas au bon indice.");
+        assertEquals(arc2, liste.get(1), "Le second arc ajouté n'est pas au bon indice.");
+    }
+
+    @Test
+    public void test3_toString_liste_vide() {
+        // Given
+        Arcs arcs = new Arcs();
+
+        // When
+        String resultat = arcs.toString();
+
+        // Then
+        assertEquals("", resultat, "Le toString d'une liste vide devrait retourner une chaîne vide.");
+    }
+
+    @Test
+    public void test4_toString_plusieurs_arcs() {
+        // Given
+        Arcs arcs = new Arcs();
+        arcs.ajouterArc(new Arc("B", 12.0));
+        arcs.ajouterArc(new Arc("D", 87.0));
+
+        // When
+        String resultat = arcs.toString();
+
+        // Then
+        // On s'assure qu'il y a bien un espace entre les arcs et pas d'espace en trop à la fin
+        assertEquals("B(12.0) D(87.0)", resultat, "La méthode toString doit séparer les arcs par un espace.");
+    }
+}
