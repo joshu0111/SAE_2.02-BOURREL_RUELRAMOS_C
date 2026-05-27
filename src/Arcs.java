db80fba3d17b543c11da747ef56023d179d150b3
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-05-27 08:32:57 +0200

    Implementation de la classe Arcs

--- src/Arcs.java
+++ src/Arcs.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Arcs {
+    /** Attributs */
+    ArrayList<Arc> listeArc;
+
+    /**
+     * Constructeur vide
+     */
+    public Arcs(){
+        this.listeArc = new ArrayList<Arc>();
+    }
+
+    /**
+     * Methode qui permet d'ajojuter un arc la liste des arcs
+     * @param a Objet de type Arc
+     */
+    public void ajouterArc(Arc a){
+        this.listeArc.add(a);
+    }
+
+    /**
+     * Methode qui permet de recuperer la liste des arcs
+     * @return ArrayList<Arc>
+     */
+    public ArrayList<Arc> getListeArc() {
+        return listeArc;
+    }
 }
