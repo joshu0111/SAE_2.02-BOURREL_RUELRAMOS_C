428632b74de92bb08bc9d2b713cd80e39bbf62cf
Robin Bourrel <robin.bourrel3@etu.univ-lorraine.fr>
2026-05-27 09:28:11 +0200

    Ajout des commentaires de la classe graphe

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,9 +1,19 @@
 import java.util.ArrayList;
 
 /**
- *
+ * Interface graphe
  */
 public interface Graphe {
+    /**
+     * Methode qui permet d'avoir la liste de tout les noeuds du graphe
+     * @return la liste des noeuds du graphe
+     */
     public ArrayList<String> getListeNoeuds();
+
+    /**
+     * Méthode qui permet d'avoir les noeuds adjacents correspondant au noeud
+     * @param noeud noeud que l'on souhaite obtenir les neouds adjacents
+     * @return les noeuds adjacents
+     */
     public Arcs getNoeudAdjacence(String noeud);
 }
