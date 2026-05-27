eca9d58bee47d183d3f428630c44edfdc224515f
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-05-27 09:30:57 +0200

    Implementation class GrapheListe

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -1,2 +1,20 @@
-public class GrapheListe {
+import java.util.ArrayList;
+
+public class GrapheListe implements Graphe{
+    ArrayList<String> noeuds;
+    ArrayList<Arcs> adjacence;
+
+    @Override
+    public ArrayList<String> getListeNoeuds(){
+        return this.noeuds;
+    }
+
+    @Override
+    public Arcs getNoeudAdjacence(String noeud){
+        int indiceNoeud = this.noeuds.indexOf(noeud);
+        if (indiceNoeud < 0){
+            return null;
+        }
+        return this.adjacence.get(indiceNoeud);
+    }
 }
