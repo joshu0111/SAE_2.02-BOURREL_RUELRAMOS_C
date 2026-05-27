3150082b92cf4142c576aa3398b09d451be3a82d
Robin Bourrel <robin.bourrel3@etu.univ-lorraine.fr>
2026-05-27 09:17:37 +0200

    Modification de la classe Arc

--- src/Arc.java
+++ src/Arc.java
@@ -1,14 +1,14 @@
 public class Arc {
     /** Attributrs */
     String nCible;
-    int poids;
+    double poids;
 
     /**
      * Constructeur
      * @param n Noeud cible de l'arc (chaine de caractere)
      * @param p Entier correspondant au poids de l'arc
      */
-    public Arc(String n, int p){
+    public Arc(String n, double p){
         this.nCible = n;
         this.poids = p;
     }
