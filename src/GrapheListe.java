7d697d33aa30753de2ea8a3c0f0e79f7ee0bd5c5
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-05-27 09:49:10 +0200

    Implementation de la methode ajouterArc de la class GrapheList

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class GrapheListe implements Graphe{
     ArrayList<String> noeuds;
-    ArrayList<Arcs> adjacence;
+    ArrayList<Arcs> adjacences;
 
     @Override
     public ArrayList<String> getListeNoeuds(){
@@ -15,6 +15,27 @@ public class GrapheListe implements Graphe{
         if (indiceNoeud < 0){
             return null;
         }
-        return this.adjacence.get(indiceNoeud);
+        return this.adjacences.get(indiceNoeud);
+    }
+
+    /**
+     * Methode qui permet d'ajouter un arc au graphe
+     * @param noeudSrc String qui correspond a l'identifiant du noeud
+     * @param noeudDest String qui correspond a l'identifiant du noeud
+     * @param poids Double qui corredpond au poids de l'arc
+     */
+    public void ajouterArc(String noeudSrc, String noeudDest, double poids){
+        // Indices des noeuds
+        int indiceSrc = this.noeuds.indexOf(noeudSrc);
+        int indiceDest = this.noeuds.indexOf(noeudDest);
+
+        // Verifications des parametres
+        if ((indiceSrc > 0) && (indiceDest > 0) && (poids>0)){
+            // Creation d'un nouvel arc
+            Arc a = new Arc(noeudDest, poids);
+
+            // Ajout aux adjacences
+            this.adjacences.get(indiceSrc).ajouterArc(a);
+        }
     }
 }
