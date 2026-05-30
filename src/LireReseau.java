52ba829458349eeef6b722123aa7be2480f8ce90
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-05-30 10:30:45 +0200

    Implementation de la class LireReseau et creation de la class MainTransport

--- src/LireReseau.java
+++ src/LireReseau.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LireReseau {
+    public static Graphe lire(String fichier_stations, String fichier_connexions) throws IOException {
+
+        // Init des lecteurs
+        BufferedReader noeuds = new BufferedReader(new FileReader(fichier_stations));
+        BufferedReader liens = new BufferedReader(new FileReader(fichier_connexions));
+
+        // Init des variables
+        String ligneLien;
+        String [] tabLigneLien;
+
+        // Init du graphe
+        GrapheListe g = new GrapheListe();
+
+        // Boucles qui lit chaque ligne et qui creer un nouvel arc dans le graphe
+        while ((ligneLien = liens.readLine()) != null){
+            tabLigneLien = ligneLien.split(";");
+            g.ajouterArc(
+                    tabLigneLien[0],
+                    tabLigneLien[1],
+                    Double.parseDouble(tabLigneLien[2]));
+        }
+        return g;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(lire("infos_reseau/stan.nodes.txt", "infos_reseau/stan.edges.txt"));
+    }
 }
