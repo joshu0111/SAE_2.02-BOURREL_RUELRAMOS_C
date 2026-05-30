2f67a0c7cd209b9b31704b45cf088cf6c2236dc8
Joshua Ruel Ramos <joshua.ruel-ramos3@etu.univ-lorraine.fr>
2026-05-30 14:21:25 +0200

    Correction sur MainTransport

--- src/MainTransport.java
+++ src/MainTransport.java
@@ -3,41 +3,53 @@ import java.util.List;
 
 public class MainTransport {
     public static void main(String[] args) throws IOException {
-        if (args.length < 2){
-            System.out.println("Utilisation du programme : \n     java -jar ApplicationCalculTajetTransport.jar [id_arret_depart] [id_arret_destination]");
-            return;
+        try {
+            if (args.length < 2){
+                System.out.println("Utilisation du programme : \n     java -jar ApplicationCalculTajetTransport.jar [id_arret_depart] [id_arret_destination]");
+                return;
+            }
+            String depart = args[0].split("\\[")[1].split("]")[0];
+            String destination = args[1].split("\\[")[1].split("]")[0];
+
+            // Creation du graphe
+            Graphe graphe = LireReseau.lire(
+                    "infos_reseau/stan.nodes.txt",
+                    "infos_reseau/stan.edges.txt");
+
+
+            // Calcul BellmanFord
+            BellmanFord bellmanFord = new BellmanFord();
+
+            long startTime = System.nanoTime();
+            Valeurs valeursBellmanFord = bellmanFord.resoudre(graphe, depart);
+            long endTime = System.nanoTime();
+
+            List<String> cheminBellmanFord = valeursBellmanFord.calculerChemin(destination);
+
+//        System.out.println(cheminBellmanFord + "\nCalculé en " + (endTime-startTime) + " ns avec l'algorithme de Bellaman Ford.");
+
+
+            // Calcul Djikstra
+            Djikstra djikstra = new Djikstra();
+
+            startTime = System.nanoTime();
+            Valeurs valeursDjikstra = djikstra.resoudre(graphe, depart);
+            endTime = System.nanoTime();
+
+            List<String> cheminDjikstra = valeursDjikstra.calculerChemin(destination);
+
+//        System.out.println(cheminDjikstra + "\nCalculé en " + (endTime-startTime) + " ns avec l'algorithme de Djikstra.");
+
+            // On transforme la liste [arret1, arret2] en une chaîne "arret1;arret2"
+            String formatPython = String.join(";", cheminDjikstra);
+
+            // On affiche UNIQUEMENT cette chaîne pour que Python la lise
+            System.out.print(formatPython);
+
+            System.out.flush();
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        String depart = args[0].split("\\[")[1].split("]")[0];
-        String destination = args[1].split("\\[")[1].split("]")[0];
-
-        // Creation du graphe
-        Graphe graphe = LireReseau.lire(
-                "infos_reseau/stan.nodes.txt",
-                "infos_reseau/stan.edges.txt");
-
-
-        // Calcul BellmanFord
-        BellmanFord bellmanFord = new BellmanFord();
-
-        long startTime = System.nanoTime();
-        Valeurs valeursBellmanFord = bellmanFord.resoudre(graphe, depart);
-        long endTime = System.nanoTime();
-
-        List<String> cheminBellmanFord = valeursBellmanFord.calculerChemin(destination);
-
-        System.out.println(cheminBellmanFord + "\nCalculé en " + (endTime-startTime) + " ns avec l'algorithme de Bellaman Ford.");
-
-
-        // Calcul Djikstra
-        Djikstra djikstra = new Djikstra();
-
-        startTime = System.nanoTime();
-        Valeurs valeursDjikstra = djikstra.resoudre(graphe, depart);
-        endTime = System.nanoTime();
-
-        List<String> cheminDjikstra = valeursDjikstra.calculerChemin(destination);
-
-        System.out.println(cheminDjikstra + "\nCalculé en " + (endTime-startTime) + " ns avec l'algorithme de Djikstra.");
-        return;
     }
 }
